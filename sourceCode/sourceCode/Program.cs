6923af63fd4dcd5376bb12fe11dbd641860b0549
Trần Minh Tuyên <trminhtuyen1905@gmail.com>
2024-11-12 22:20:12 +0700

    sumArray
    tinh tong cua mang

---
+++ sourceCode/sourceCode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sourceCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Nhap mang Array[{i}]: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int tong = sumArray(array);
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.Write(array[j] + " ");               
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Tong cua mang la: {tong}");
+            Console.ReadKey();
+        }
+        static int sumArray (int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
