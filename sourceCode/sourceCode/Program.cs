a105ee5a42800fc7c6222c2ab5a2e6ee1607ae75
Trần Minh Tuyên <trminhtuyen1905@gmail.com>
2024-11-14 20:02:33 +0700

    một vài bài tập tính tổng mảng

--- sourceCode/sourceCode/Program.cs
+++ sourceCode/sourceCode/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+//tinh tong cac phan tu trong mang
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,3 +40,99 @@ namespace sourceCode
         }
     }
 }
+Console.Write("--------------------------------------------------------------------------");
+
+// tinh tong chan cua mang
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sourceCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Nhap mang Array[{i}]: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int tongChan = sumOfEven(array);
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.Write(array[j] + " ");               
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Tong cua mang la: {tongChan}");
+            Console.ReadKey();
+        }
+        static int sumOfEven (int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] %2 == 0)
+                {
+                    sum += array[i];
+                }
+                
+            }
+            return sum;
+        }
+    }
+}
+
+Console.Write("-------------------------------------------------------------------");
+
+// tinh tong le cua mang
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinhSoLeCuaMang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Nhap phan tu cua mang a[{i}]: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int tongLe = sumOfOdd(array);
+            Console.WriteLine($"Tong le cua mang la: {tongLe} ");
+            Console.ReadKey();
+        }
+        static int sumOfOdd(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++ )
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
+
+Console.Write("-----------------------------------------------------------------------------------");
+
+
+
